5cb201badecd51e65be8f285423874a6f5360668
kmarks2013 <kmarks2013@gmail.com>
2020-04-01 19:28:07 -0400

    adds index, create, and show methods for ScoresController

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -1,2 +1,17 @@
 class ScoresController < ApplicationController
+
+    def index
+        render json: Score.all
+    end
+
+    def create
+        score = Score.create()
+        render json: score
+    end
+
+    def show
+        score = Score.find(params[:id])
+        render json: score
+    end
+
 end
