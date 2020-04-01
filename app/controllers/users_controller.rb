f51c268f3a5833562ffbf6e71b2a2ad304ae0041
kmarks2013 <kmarks2013@gmail.com>
2020-04-01 19:16:54 -0400

    adds create and show methods for Users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,12 @@
 class UsersController < ApplicationController
+
+    def create
+        User.create(:username)
+    end
+
+    def show
+        user= User.find(:params[:useranme])
+        render json: user
+    end
+
 end
