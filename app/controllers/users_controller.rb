b954e622078de5a962fa62d37d8fac9e2ee80c2f
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-04-01 22:28:37 -0400

    updates user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,23 @@
 class UsersController < ApplicationController
 
+    def index
+        users = User.all
+        render json: users
+    end
+
     def create
-        User.create(:username)
+      user =  User.create(user_params)
+      render json: user
     end
 
     def show
-        user= User.find(:params[:useranme])
+        user= User.find(:params[:id])
         render json: user
     end
 
+    private
+
+    def user_params
+        params.permit(:username)
+    end
 end
