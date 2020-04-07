615faf085a500a2b7541bf7a2556fc5f82c8dfc8
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-04-06 23:02:45 -0400

    adds score_params

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -6,13 +6,19 @@ class ScoresController < ApplicationController
     end
 
     def create
-        score = Score.create()
+        score = Score.create(score_params)
         render json: score
     end
 
     def show
         score = Score.find(params[:id])
         render json: score
+    end
+
+    private
+
+    def score_params
+        params.permit(:score, :user_id)
     end
 
 end
